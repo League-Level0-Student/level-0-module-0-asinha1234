563223eb82a99d91b5878a7c5acb20fdf7eea0bc
root <root@7ce554cb71db>
2023-03-14 03:03:46 +0000

    madlibs greeter and world domanation

--- src/_03_print_and_popups/_3_world_domination/WorldDomination.java
+++ src/_03_print_and_popups/_3_world_domination/WorldDomination.java
@@ -8,11 +8,21 @@ public class WorldDomination {
 	public static void main(String[] args) {
 		
 		// 1. Ask the user if they know how to write code.
-
+JOptionPane.showInputDialog("do you know how to write code?");
 		// 2. If they say "yes", tell them they will rule the world.
+	if ("yes" != null) 
+	{
+		JOptionPane.showMessageDialog(null,"You will rule the world!");
+		
+	}
 
 		// 3. Otherwise, tell them to sign up for classes at The League.
+if  ("no") {
+	
+} else { 
+	JOptionPane.showMessageDialog(null, "sign up for classes at the league!");
 
+}
 	}
 }
 
